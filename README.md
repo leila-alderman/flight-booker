c1754979503a8931fba9a110227db2884a2536cc
leila-alderman <leila@spreedly.com>
2019-04-29 15:42:04 -0400

    Add Flight and Airport models and seed data

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+This Flight Booker project allows users to search for and book flights.
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+[This project](https://www.theodinproject.com/courses/ruby-on-rails/lessons/building-advanced-forms?ref=lnav) is part of [The Odin Project curriculum](https://www.theodinproject.com/courses?ref=homenav).
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Airport.create(code: "ATL", name: "Hartsfield-Jackson Atlanta International Airport")
+Airport.create(code: "LAX", name: "Los Angeles International Airport")
+Airport.create(code: "ORD", name: "O'Hare International Airport")
+Airport.create(code: "DFW", name: "Dallas/Fort Worth International Airport")
+Airport.create(code: "DEN", name: "Denver International Airport")
+Airport.create(code: "JFK", name: "John F. Kennedy International Airport")
+Airport.create(code: "SFO", name: "San Francisco International Airport")
+
+arr = (1..7).to_a
+perms = arr.permutation(2).to_a
+perms.each do |perm|
+    10.times do |n|
+        Flight.create(from_airport: Airport.find(perm[0]), 
+            to_airport: Airport.find(perm[1]),
+            departure: Time.now + n.days, 
+            duration: "#{1 + rand(9)} hours #{1 + rand(59)} minutes")
+    end
+end
