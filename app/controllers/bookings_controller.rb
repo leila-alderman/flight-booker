37619c70952b2554544b96462bbbaa98a32dca78
leila-alderman <leila@spreedly.com>
2019-05-01 17:26:45 -0400

    Add new booking form and update controller methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+
+  @@token = "ROvQPckMbDyyE3cBYKpZCmrHYpK"
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+  
+  def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = @flight.bookings.build
+    @num_passengers = params[:num_passengers].to_i
+  end
+
+  def create
+    @flight = Flight.find(params[:flight_id])
+    @booking = @flight.bookings.create(booking_params)
+    redirect_to [@flight, @booking]
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:num_passengers)
+    end
+
 end
