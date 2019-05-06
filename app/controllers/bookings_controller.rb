21a03e64d6e082034a392173b603a52a5612e5b2
leila-alderman <leila@spreedly.com>
2019-05-06 10:08:18 -0400

    Successfully allow purchases using the Spreedly test gateway

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,31 +17,23 @@ class BookingsController < ApplicationController
     # If user has just completed filling out the Spreedly Express pop-up form, 
     # then the URL will contain the payment_method_token query parameter
     if params[:payment_method_token]
-      @payment_method_token = params[:payment_method_token]
-      data = {
-        transaction: {
-          payment_method_token: @payment_method_token,
-          amount: @total_price * 100,
-          currency_code: "USD",
-          retain_on_success: false
-        }
-      }
-      purchase_response = `curl https://core.spreedly.com/v1/gateways/#{ENV["TEST_GATEWAY_TOKEN"]}/purchase.json \
-        -u "#{ ENV["ENV_KEY"] }:#{ ENV["ACCESS_SECRET"] }" \
-        -H 'Content-Type: application/json' \
-        -d #{data.to_json}`
-      full_response = %x{curl https://core.spreedly.com/v1/gateways/ROvQPckMbDyyE3cBYKpZCmrHYpK/purchase.json -u "ZsZlG3TIExW3qlEs5lOVIj4mEyN:UoN13oQN4oSWkN8zW87FvJuqT4efLdJJtLz5tpUCTK4a81V4v4OXC1AFUKIsZsN2" -H 'Content-Type: application/json' -d "{\"transaction\":{\"payment_method_token\":\"CYttCMCjt2ARpexVo3ewSXhExX5\",\"amount\":228800,\"currency_code\":\"USD\",\"retain_on_success\":false}}"}
-      binding.pry
-      # purchase_response = JSON.parse purchase_response
-      if purchase_response.transaction.succeeded == true
+      payment_method_token = params[:payment_method_token]
+
+      env = Spreedly::Environment.new(ENV["ENV_KEY"], ENV["ACCESS_SECRET"])
+      transaction = env.purchase_on_gateway(ENV["TEST_GATEWAY_TOKEN"], # gateway token for the test API gateway
+        payment_method_token, # payment method token from the query parameters
+        @total_price * 100)    # purchase amount converted into cents
+
+      if transaction.succeeded?
         @booking.num_passengers = @num_passengers
         @booking.total_price = @total_price
+        @booking.transaction_token = transaction.token
         @booking.save
         flash[:success] = "Payment successful!"
         redirect_to flight_booking_path(@flight, @booking)
       else
-        flash[:danger] = "Payment failed."
-        redirect_to new_flight_booking(@flight)
+        flash[:danger] = "Payment failed. Please try again."
+        redirect_to new_flight_booking_path(@flight, num_passengers: @num_passengers)
       end
     end
   end
